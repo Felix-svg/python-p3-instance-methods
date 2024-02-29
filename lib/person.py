ee00ae396d6e5bc1721e0e8e752ff6972349440c
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-02-29 08:49:37 +0300

    Completed assignment

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,13 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+        
+
+person_x = Person()
+person_x.talk()
+person_x.walk()
